5b3251907b021e71edbab9ed9e73baf1b342845e
julioett <julio@estuturno.app>
2024-02-05 17:34:56 -0300

    fix

--- src/views/UserQueueBooking.vue
+++ src/views/UserQueueBooking.vue
@@ -202,7 +202,13 @@ export default {
                   <span class="booking-details-title"> {{ $t("userQueueBooking.toGoal.1") }}* </span><br>
                   <span class="booking-details-content"> <i class="bi bi-person"></i> {{ getBeforeYou() }} </span><br>
                 </div>
-                <div v-if=" state.beforeYou >= 0" class="col-6 booking-details-card">
+                <div
+                  v-if="state.booking.block && state.booking.block.hourFrom"
+                  class="col-6 booking-details-card">
+                  <span class="booking-details-title"> {{ $t("userQueueBooking.blockInfo") }}</span><br>
+                  <strong>{{ state.booking.block.hourFrom }} - {{ state.booking.block.hourTo }}</strong>
+                </div>
+                <div v-else-if="state.beforeYou >= 0" class="col-6 booking-details-card">
                   <span class="booking-details-title"> {{ $t("userQueueBooking.estimatedTime") }}* </span><br>
                   <span class="booking-details-content"> <i class="bi bi-stopwatch"></i> {{ getEstimatedTime() }} </span> <br>
                 </div>
@@ -223,13 +229,7 @@ export default {
                     </h6>
                   </div>
                   <div
-                    v-if="state.booking.block && state.booking.block.hourFrom"
-                    class="booking-notification-title">
-                    {{ $t("userQueueBooking.blockInfo") }}
-                    <strong>{{ state.booking.block.hourFrom }} - {{ state.booking.block.hourTo }}</strong>
-                  </div>
-                  <div
-                    v-else-if="state.commerce.serviceInfo && state.commerce.serviceInfo.attentionHourFrom"
+                    v-if="state.commerce.serviceInfo && state.commerce.serviceInfo.attentionHourFrom"
                     class="booking-notification-title">
                     {{ $t("userQueueBooking.operationStart") }}
                     <strong>{{ state.commerce.serviceInfo.attentionHourFrom }}:00</strong>
