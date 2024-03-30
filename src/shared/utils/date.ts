44fa0bf7d3a8e8588fa6c6617bd8b149780eea53
julioett <julio@estuturno.app>
2024-03-29 23:02:05 -0300

    fix

--- src/shared/utils/date.ts
+++ src/shared/utils/date.ts
@@ -6,10 +6,6 @@ export const dateYYYYMMDD = (date: Date) => {
 }
 
 export const getDate = (date, timeZoneIn) => {
-  const dateCorrected = new Date(
-  new Date(date).toLocaleString('en-US', {
-    timeZone: timeZoneIn,
-  }));
-  const [year, month, day] = dateCorrected.toISOString().slice(0,10).split('-');
+  const [year, month, day] = date.toISOString().slice(0,10).split('-');
   return `${day}/${month}/${year}`;
 }
